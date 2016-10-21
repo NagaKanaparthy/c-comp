50987ad3c603a5b8222b33b7e911e1b648e19e1b
NagaKanparthy <nkanaparthyper@live.com>
2016-10-20 20:04:22 -0400

    finished mutliple rule fuse

--- prj2/generator.py
+++ prj2/generator.py
@@ -1,14 +1,15 @@
 import sys
 from token import Token
 class RuleDictionary:
-   def __init__(self,rulesList):
-	self.rulesDictionary = {}
+	def __init__(self,rulesList):
+		self.rulesDictionary = {}
 	for rule in rulesList:
-            if(self.rulesDictionary.get(rule.name) == None):
-                self.rulesDictionary.update({rule.name:rule})
-            else:
-                print(rule.toString())
-		
+		ruleInDict = self.rulesDictionary.get(rule.name)
+		if(ruleInDict == None):
+			self.rulesDictionary.update({rule.name:rule})
+		else:
+			#add fuse rules and flag rule if confilct in predict sets
+			ruleInDict.fuseRules(rule)
 class Rule:
     def __init__(self,ruleTokenString):
         self.confilct = False
@@ -16,6 +17,16 @@ class Rule:
         dataList = ruleTokenString.split('|')
         self.name = dataList[0]
         self.subRules = {dataList[1] : SubRule(dataList[1],dataList[2])}
+	def fuseRules(self,rule):
+		for subRuleToFuse in rule.subRules.values:
+			for subRuleInDict in self.subRules.values:
+				for token in subRuleInDict.predicitTokens:
+					for tokenFuse in subRuleToFuse.predicitTokens:
+						if(token == tokenFuse):
+							self.conflict = True
+							self.confilctingSubRules.append(subRuleToFuse)
+							self.confilctingSubRules.append(subRuleInDict)
+			self.subRules.update({subRuleToFuse.rule:subRuleToFuse})
     def toString(self):
         string = "Rule:"+self.name+'\n'
         for srule in self.subRules.values():
@@ -24,11 +35,11 @@ class Rule:
 class SubRule:
     def __init__(self, rule, predicitTokenString):
         self.rule = rule
-        self.predicitTokens = {}
+        self.predicitTokens = []
         for token in predicitTokenString.split():
-            self.predicitTokens.update({token:token})
+            self.predicitTokens.append(token)
     def toString(self):
-        return self.rule+"|"+str(self.predicitTokens.keys())
+        return self.rule+"|"+str(self.predicitTokens)
 #from rule import RuleDictionary
 #from rule import Rule
 #from rule import SubRule
